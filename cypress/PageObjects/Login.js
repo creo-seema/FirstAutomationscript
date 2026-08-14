687ae0deb114bb6aebdb203e2a3cc025749f097b
Seema Bhardwaj <seema.bhardwaj@ad.creospan.com>
2026-08-14 16:39:17 +0530

    Launch the application

---
+++ cypress/PageObjects/Login.js
@@ -0,0 +1,18 @@
+class Login {
+
+    setUserName(username){
+        cy.get("#user-name").type(username);
+
+    }
+    setPassword(password){
+
+      cy.get("#password").type(password);
+
+    }
+    clickLogin(){
+        cy.get("#login-button").click();
+
+
+    }
+}
+export default Login;
