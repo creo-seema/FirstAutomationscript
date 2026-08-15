4503ef506f260d1d92933a2d0c2fee23287fd360
Seema Bhardwaj <seema.bhardwaj@ad.creospan.com>
2026-08-15 15:35:39 +0530

    Add mochawesome reporter config and dependencies

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -4,7 +4,7 @@ import Login from "../PageObjects/Login";
 describe('Login to the application', () => {
   it('launch the application', () => {
     cy.visit('https://www.saucedemo.com/')
-    const ln = new Login();
+    const ln =  new Login();
     ln.setUserName("standard_user");
     ln.setPassword("secret_sauce");
     ln.clickLogin();
