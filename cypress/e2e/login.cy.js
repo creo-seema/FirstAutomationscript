d50c511c54c9c1f7bde3cf1ecfaba425fd1f8876
Seema Bhardwaj <seema.bhardwaj@ad.creospan.com>
2026-08-16 15:29:33 +0530

    updated suite

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,8 +5,8 @@ describe('Login to the application', () => {
   it('launch the application', () => {
     cy.visit('https://www.saucedemo.com/')
     const ln = new Login();
-    ln.setUserName("standard_user");
-    ln.setPassword("secret_sauce");
+    ln.setUserName("standard_user") ;
+    ln.setPassword("secret_sauce") ;
     ln.clickLogin()  ;
 
   })
