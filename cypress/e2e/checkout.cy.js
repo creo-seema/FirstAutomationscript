e310ba847607fca2ee2c35f07732f3ca01ec07cc
Seema Bhardwaj <seema.bhardwaj@ad.creospan.com>
2026-08-17 17:06:29 +0530

    Added fixture file for data

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -5,24 +5,26 @@ import Checkout from "../PageObjects/Checkout";
 
 describe('Checkout flow', () => {
   it('adds a product to cart and completes checkout', () => {
-    cy.visit('https://www.saucedemo.com/')
+    cy.fixture('checkoutData').then((data) => {
+      cy.visit('https://www.saucedemo.com/')
 
-    const ln = new Login();
-    ln.setUserName("standard_user");
-    ln.setPassword("secret_sauce");
-    ln.clickLogin();
+      const ln = new Login();
+      ln.setUserName(data.username);
+      ln.setPassword(data.password);
+      ln.clickLogin();
 
-    const inventory = new Inventory();
-    inventory.addBackpackToCart();
-    inventory.goToCart();
+      const inventory = new Inventory();
+      inventory.addBackpackToCart();
+      inventory.goToCart();
 
-    const cart = new Cart();
-    cart.verifyProductInCart('Sauce Labs Backpack');
-    cart.goToCheckout();
+      const cart = new Cart();
+      cart.verifyProductInCart('Sauce Labs Backpack');
+      cart.goToCheckout();
 
-    const checkout = new Checkout();
-    checkout.enterInfo('Seema', 'Bhardwaj', '411001');
-    checkout.finishOrder();
-    checkout.verifyOrderComplete();
+      const checkout = new Checkout();
+      checkout.enterInfo(data.firstName, data.lastName, data.postalCode);
+      checkout.finishOrder();
+      checkout.verifyOrderComplete();
+    })
   })
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,13 @@
-
 import Login from "../PageObjects/Login";
 
 describe('Login to the application', () => {
   it('launch the application', () => {
-    cy.visit('https://www.saucedemo.com/')
-    const ln = new Login();
-    ln.setUserName("standard_user") ;
-    ln.setPassword("secret_sauce") ;
-    ln.clickLogin()  ;
-
+    cy.fixture('checkoutData').then((data) => {
+      cy.visit('https://www.saucedemo.com/')
+      const ln = new Login();
+      ln.setUserName(data.username);
+      ln.setPassword(data.password);
+      ln.clickLogin();
+    })
   })
 })
